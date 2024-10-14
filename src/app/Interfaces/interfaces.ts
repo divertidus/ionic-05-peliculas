16175b28d9c484ca0a5d156776c505eb7240e800
nacho <nacho@teconsite.com>
2024-10-14 15:35:04 +0200

    Se crea el componente detalle para mostrar los datos de las peliculas. En el mismo detalle componente se va colocando todo en html y con css obtenido del curso. El ts carga en su onInit los detalles de la pelicula y los asigna a una variable detallesPelicula. Si se presetan un error en la consola es porque cuando intentar cargar la propiedad que sea aun no está. Se arregla poniendo ? al final de cada elemento de la interfaz para hacerlo opcional. Sin terminar detalles pero si video 153

--- src/app/Interfaces/interfaces.ts
+++ src/app/Interfaces/interfaces.ts
@@ -30,32 +30,32 @@ export enum OriginalLanguage {
 }
 
 export interface DetallesPelicula {
-    adult:                 boolean;
-    backdrop_path:         string;
-    belongs_to_collection: ColeccionQuePertenece;
-    budget:                number;
-    genres:                Genero[];
-    homepage:              string;
-    id:                    number;
-    imdb_id:               string;
-    origin_country:        string[];
-    original_language:     string;
-    original_title:        string;
-    overview:              string;
-    popularity:            number;
-    poster_path:           string;
-    production_companies:  ProductionCompany[];
-    production_countries:  ProductionCountry[];
-    release_date:          Date;
-    revenue:               number;
-    runtime:               number;
-    spoken_languages:      SpokenLanguage[];
-    status:                string;
-    tagline:               string;
-    title:                 string;
-    video:                 boolean;
-    vote_average:          number;
-    vote_count:            number;
+    adult?:                 boolean;
+    backdrop_path?:         string;
+    belongs_to_collection?: ColeccionQuePertenece;
+    budget?:                number;
+    genres?:                Genero[];
+    homepage?:              string;
+    id?:                    number;
+    imdb_id?:               string;
+    origin_country?:        string[];
+    original_language?:     string;
+    original_title?:        string;
+    overview?:              string;
+    popularity?:            number;
+    poster_path?:           string;
+    production_companies?:  ProductionCompany[];
+    production_countries?:  ProductionCountry[];
+    release_date?:          Date;
+    revenue?:               number;
+    runtime?:               number;
+    spoken_languages?:      SpokenLanguage[];
+    status?:                string;
+    tagline?:               string;
+    title?:                 string;
+    video?:                 boolean;
+    vote_average?:          number;
+    vote_count?:            number;
 }
 
 export interface ColeccionQuePertenece {
@@ -90,9 +90,9 @@ export interface SpokenLanguage {
 
 
 export interface ActoresPelicula {
-    id:   number;
-    cast: Cast[];
-    crew: Cast[];
+    id?:   number;
+    cast?: Cast[];
+    crew?: Cast[];
 }
 
 export interface Cast {
--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -2,22 +2,32 @@ import { Component, Input, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
 import { MoviesService } from '../../services/movies.service';
 import { ModalController } from '@ionic/angular';
 import { ActoresPelicula, DetallesPelicula } from 'src/app/Interfaces/interfaces';
+import { ImagenPipe } from "../../pipes/imagen.pipe";
+import { NgIf } from '@angular/common';
+import { addIcons } from 'ionicons';
+import * as todosLosIconos from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   standalone: true,
   styleUrls: ['./detalle.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [ImagenPipe, NgIf]
 })
 export class DetalleComponent implements OnInit {
 
 
   @Input() idPelicula!: number
 
-  private detallesPelicula!: DetallesPelicula;
-  private actoresPelicula!: ActoresPelicula;
-  constructor(private moviesService: MoviesService, private modalCtrl: ModalController) { }
+
+  detallesPelicula: DetallesPelicula = {};
+  actoresPelicula: ActoresPelicula = {};
+
+  constructor(private moviesService: MoviesService, private modalCtrl: ModalController) {
+    addIcons(todosLosIconos)
+  }
 
   ngOnInit() {
     console.log("Se recibe ID: ", this.idPelicula)
@@ -25,12 +35,14 @@ export class DetalleComponent implements OnInit {
     this.moviesService.getPeliculaDetalles(this.idPelicula)
       .subscribe(resp => {
         console.log(resp)
+        this.detallesPelicula = resp;
       })
 
 
     this.moviesService.getPeliculaActores(this.idPelicula)
       .subscribe(resp => {
         console.log(resp)
+        this.actoresPelicula = resp;
       })
 
   }
